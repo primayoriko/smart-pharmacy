a1a8cf742187692ffe28e0ba8a46ba29294e15b0
Haruka Shirayuki <arung.agamani@gmail.com>
2020-04-12 01:30:40 +0700

    Fixed test.py for testing purposes

--- src/test.py
+++ src/test.py
@@ -1,1 +1,6 @@
-print("Test")
+"""Testing file
+
+This module is not really a module. Just a mere script used to test whether the linter is good or nay
+This script writes "Testing" into stdout. Just like that
+"""
+print("Testing")
