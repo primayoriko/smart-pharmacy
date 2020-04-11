b167613a882a15c8369ba65f0a2ac25a2b14860d
Haruka Shirayuki <arung.agamani@gmail.com>
2020-04-12 01:52:47 +0700

    fix too long string on line 3

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 """Testing file
 
-This module is not really a module. Just a mere script used to test whether the linter is good or nay
+This module is not really a module. Just a script used to test whether the linter is good or nay
 This script writes "Testing" into stdout. Just like that
 """
 print("Testing")
