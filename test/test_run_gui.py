d579a2b445d42b156963aed505bc79dc830ea934
Haruka Shirayuki <arung.agamani@gmail.com>
2020-04-13 05:25:46 +0700

    Added pytest-qt plugin

--- test/test_run_gui.py
+++ test/test_run_gui.py
@@ -11,20 +11,18 @@ sys.path.insert(1, "../src")
 
 import main
 
-app = QApplication([])
-
-def test_main():
+def test_main(qtbot):
     wt = main.AppWindow()
-    QTest.keyClicks(wt.ui.fieldNamaResep, "Pramanix")
-    QTest.keyClicks(wt.findChild(QtWidgets.QLineEdit, "field_namaObat1"), "Paracetamol")
-    QTest.keyClicks(wt.findChild(QtWidgets.QLineEdit, "field_jumlah1"), "500")
+    qtbot.keyClicks(wt.ui.fieldNamaResep, "Pramanix")
+    qtbot.keyClicks(wt.findChild(QtWidgets.QLineEdit, "field_namaObat1"), "Paracetamol")
+    qtbot.keyClicks(wt.findChild(QtWidgets.QLineEdit, "field_jumlah1"), "500")
     assert main.get_dialog_data(wt, wt.count) == {
         'nama' : 'Pramanix',
         'data' : [
             ('Paracetamol', '500', 'Gram')
         ]
     }
-    QTest.mouseClick(wt.ui.addItemsButton, QtCore.Qt.LeftButton)
+    qtbot.mouseClick(wt.ui.addItemsButton, QtCore.Qt.LeftButton)
     assert wt.count == 2
     assert main.get_dialog_data(wt, wt.count) == {
         'nama' : 'Pramanix',
@@ -32,19 +30,20 @@ def test_main():
             ('Paracetamol', '500', 'Gram'), ('','', 'Gram')
         ]
     }
-    QTest.keyClicks(wt.findChild(QtWidgets.QLineEdit, "field_namaObat2"), "Ibuprofen")
-    QTest.keyClicks(wt.findChild(QtWidgets.QLineEdit, "field_jumlah2"), "100")
+    qtbot.keyClicks(wt.findChild(QtWidgets.QLineEdit, "field_namaObat2"), "Ibuprofen")
+    qtbot.keyClicks(wt.findChild(QtWidgets.QLineEdit, "field_jumlah2"), "100")
     assert main.get_dialog_data(wt, wt.count) == {
         'nama' : 'Pramanix',
         'data' : [
             ('Paracetamol', '500', 'Gram'), ('Ibuprofen','100', 'Gram')
         ]
     }
-    QTest.keyClicks(wt.ui.fieldNamaResep, "\b\b\b\b\b\b\b")
-    QTest.keyClicks(wt.ui.fieldNamaResep, "aramex")
+    qtbot.keyClicks(wt.ui.fieldNamaResep, "\b\b\b\b\b\b\b")
+    qtbot.keyClicks(wt.ui.fieldNamaResep, "aramex")
     assert main.get_dialog_data(wt, wt.count) == {
         'nama' : 'Paramex',
         'data' : [
             ('Paracetamol', '500', 'Gram'), ('Ibuprofen','100', 'Gram')
         ]
     }
+    app = QApplication([])
