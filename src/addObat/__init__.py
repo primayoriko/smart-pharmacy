8dca90216cd8ec4c18121ba2e46a1a4bec462702
primayoriko <primayoriko@gmail.com>
2020-05-15 13:33:49 +0700

    add __init__.py

---
+++ src/addObat/__init__.py
@@ -0,0 +1,10 @@
+from PyQt5 import QtWidgets, uic
+from AddObat import AddObat
+import sys
+
+if __name__ == '__main__':
+    addObatApp = QtWidgets.QApplication(sys.argv)
+    addObatWindow = AddObat()
+    addObatWindow.connectDB('../../db/Obat.db')
+    addObatWindow.show()
+    sys.exit(addObatApp.exec_())
